12ed6064c9b8ee2e790f6ce4c9752ce820aba401
love.to.code108 <love.to.code108@gmail.com>
2024-04-06 23:17:34 +0530

    i am starting to work on the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { Button } from "@/components/ui/button"
 
 function App() {
   
   return (
-    <div>
-      <Button> Hey is this button working </Button>
+    <div className=" bg-black w-[100%] h-[100vh]">
+      <MainApp/>
     </div>
   )
+}
+
+
+
+function MainApp(){
+
+
+  return(
+    <div className=" text-white">
+      
+
+      {/* THE SIGN UP BUTTON */}
+      
+
+
+      {/* THE SIGN IN BUTTON */}
+
+
+
+      {/* THE MAIN HEADING */}
+
+
+
+      {/* THE SUB HEADING */}
+
+
+
+      {/* THE IMAGE */}
+
+
+
+    </div>
+  );
 }
 
 export default App
