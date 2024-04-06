d9a959f11171e97ff51baa2a9c4fbc8b72cec484
love.to.code108 <love.to.code108@gmail.com>
2024-04-07 01:04:40 +0530

    the second part is done

--- src/App.jsx
+++ src/App.jsx
@@ -74,6 +74,57 @@ function MainApp() {
       </div>
 
 
+
+
+
+
+
+
+
+      {/* PART 2 */}
+      <div className=" mb-[8rem] zIndex-2 relative bottom-[4rem]">
+
+
+
+
+
+      {/* THE SECOND HEADING STREET LIGHT FAULT DETECTOR  */}
+        <div className=" w-[100%] font-55 font-inter flex justify-center mb-8">
+          <h1>Street Light Fault Detector</h1>
+        </div>
+
+
+        {/* THE FIRST PARAGRAPH */}
+        <div className=" w-[100%] font-13 font-inter flex justify-center mb-8">
+          <p className=" w-[1166px] text-center">Electricity is the critical need for progress of the livelihood.ln many Indian cities, the maintenance of street lights has become a challenging and inefficient process due to the lack of a centralized monitoring system. ldentifying faults, such as non- functioning lights, current leakage and cable breakage, relies on citizen grievances, leading to delays, increased costs, and safety concerns. Linemen spend valuable time manually searching for faults, diagnosing issues, and fixing them, which can take several days to complete. The absence of precise fault location information further complicates the process. To overcome these obstacles, we seek an innovative solution that provides realtime fault detection, accurate identification of fault types, and precise location tracking of faulty street lights. This solution aims to empower linemen with efficient fault management capabilities, reducing their workload and ensuring timely maintenance. Moreover, it should enable the local authorities to proactively address faults, enhance service quality, and optimize street light maintenance processes in their respective cities..The prime aim of this problem statement is to develop a Automated Defect Detection and Prevention Assistance with Effective Governance for Cities in India</p>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+      {/* PART 3 */}
+
+      {/* HEADING FOR STATUS OF STREET LIGHT NEAR YOU */}
+
+
+
+
+      {/* CHOOSE YOUR LOCATION */}
+
+
+
+
+      {/* TABLE FOR WORKING STREET LIGHT AND FAULTY STREET LIGHT */}
+
+
+
+
+
     </div>
   );
 }
