fa75472ae87f0771ef08e85c74b25f38a271b6dd
love.to.code108 <love.to.code108@gmail.com>
2024-04-07 08:57:43 +0530

    the sign in page is done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,42 +1,83 @@
 import "../app.css"
 import earthImg from "../assets/JPG/earthLightFromSpace.jpg"
 
+// CHADCN UI INPUTS
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
+
 
 export const UserLogin = () => {
     return (
-        <div className=" w-[100%] h-[100vh] bg-black">
+        <div className=" w-[100%] h-[100vh] bg-black flex justify-center items-center">
+
+
+
+
+            <div className=" font-inter flex flex-col items-start">
+                {/* THE SIGN IN HEADING */}
+                <div className=" mb-4">
+                    <h1 className=" text-white font-34">Sign In</h1>
+                </div>
+
+
+
+
+
+
+                <div>
+                    {/* THE USERNAME INPUT SECTION */}
+                    <div className=" mb-6">
+                        <Input className=" rounded-none font-inter w-[21rem]"
+                            placeholder="Username" />
+                    </div>
+
+
+
+
+                    {/* THE PASSWORD INPUT SECTION */}
+                    <div className=" mb-6">
+                        <Input className=" rounded-none font-inter w-[21rem] "
+                            type="password"
+                            placeholder="Password" />
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+                <div className=" flex w-[100%] justify-between">
+
+                    {/* ALREADY HAVE AN ACCOUNT SIGN IN  */}
+                    <div className=" text-white font-13 ">
+                        <p>Already have an account ?</p>
+                            <p className=" text-red-500  relative bottom-1 hover:cursor-pointer">Sign In</p>
+                    </div>
+
+
+                    {/* THE SIGN UP BUTTON */}
+                    <div>
+                        <Button className=" bg-white text-black rounded-none font-inter
+                        h-[2rem] w-[6rem] hover:text-white hover:bg-black hover:border-2 hover:border-white ">Sign In</Button>
+                    </div>
+
+                </div>
+            </div>
+
+
+
 
             {/* EARTH IMAGE */}
-            <img src={earthImg} alt="" />
-
-
-
-            {/* THE SIGN IN HEADING */}
             <div>
-                <h1>Sign In</h1>
-            </div>
-
-
-            <div>
-                {/* THE USERNAME INPUT SECTION */}
-                
-
-
-
-                {/* THE PASSWORD INPUT SECTION */}
-            </div>
-
-
-
-            <div>
-                {/* THE SIGN UP BUTTON */}
-
-
-
-
-                {/* ALREADY HAVE AN ACCOUNT SIGN IN  */}
-
-
+                <img className=" w-[40rem]" src={earthImg} alt="" />
             </div>
         </div>
     )
