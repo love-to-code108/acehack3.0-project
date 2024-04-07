c2d51b6fe4cc20405694625eed17741d47b881f2
love.to.code108 <love.to.code108@gmail.com>
2024-04-07 08:32:54 +0530

    started working on the sign in page

---
+++ src/pages/login.jsx
@@ -0,0 +1,43 @@
+import "../app.css"
+import earthImg from "../assets/JPG/earthLightFromSpace.jpg"
+
+
+export const UserLogin = () => {
+    return (
+        <div className=" w-[100%] h-[100vh] bg-black">
+
+            {/* EARTH IMAGE */}
+            <img src={earthImg} alt="" />
+
+
+
+            {/* THE SIGN IN HEADING */}
+            <div>
+                <h1>Sign In</h1>
+            </div>
+
+
+            <div>
+                {/* THE USERNAME INPUT SECTION */}
+                
+
+
+
+                {/* THE PASSWORD INPUT SECTION */}
+            </div>
+
+
+
+            <div>
+                {/* THE SIGN UP BUTTON */}
+
+
+
+
+                {/* ALREADY HAVE AN ACCOUNT SIGN IN  */}
+
+
+            </div>
+        </div>
+    )
+}
